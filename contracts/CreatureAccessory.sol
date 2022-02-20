a5670bf3d0d27abc5d15235c1b7f3ff796f8ffe7
Martin Roy <94478822+CryptoRPG-World@users.noreply.github.com>
2022-02-20 13:42:49 -0500

    Update CreatureAccessory.sol

--- contracts/CreatureAccessory.sol
+++ contracts/CreatureAccessory.sol
@@ -5,14 +5,14 @@ pragma solidity ^0.8.0;
 import "./ERC1155Tradable.sol";
 
 /**
- * @title CreatureAccessory
- * CreatureAccessory - a contract for Creature Accessory semi-fungible tokens.
+ * @title NPCAccessory
+ * NPCAccessory - a contract for NPC Accessory semi-fungible tokens.
  */
 contract CreatureAccessory is ERC1155Tradable {
     constructor(address _proxyRegistryAddress)
         ERC1155Tradable(
-            "OpenSea Creature Accessory",
-            "OSCA",
+            "CryptoRPG.world NPC Accessory",
+            "NPCA",
             "https://creatures-api.opensea.io/api/accessory/{id}",
             _proxyRegistryAddress
         ) {}
