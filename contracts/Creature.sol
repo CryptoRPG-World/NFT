f67731d98cdebe1a9f63ffb1a2b61ba169f79a4f
Martin Roy <94478822+CryptoRPG-World@users.noreply.github.com>
2022-02-20 13:41:21 -0500

    Update Creature.sol

--- contracts/Creature.sol
+++ contracts/Creature.sol
@@ -5,8 +5,8 @@ pragma solidity ^0.8.0;
 import "./ERC721Tradable.sol";
 
 /**
- * @title Creature
- * Creature - a contract for my non-fungible creatures.
+ * @title NPC
+ * Creature - a contract for my non-fungible NPCs.
  */
 contract Creature is ERC721Tradable {
     constructor(address _proxyRegistryAddress)
